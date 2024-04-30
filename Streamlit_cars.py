9ba762f6187511280bbf333ec39c4750fb83e962
Alain Recuze <144781060+El-alish@users.noreply.github.com>
2024-04-30 12:39:38 +0200

    Update Streamlit_cars.py

--- Streamlit_cars.py
+++ Streamlit_cars.py
@@ -7,10 +7,13 @@ df = pd.read_csv(link, sep=',')
 dfdrop = df.drop(['continent'], axis=1)
 tab1, tab2, tab3 = st.tabs(["DataFrame Complet", "HeatMap", "Filtre"])
 with tab1: 
-    st.write('DataFrame Complet')
-    st.dataframe(df)
-    
-    
+    continent = st.sidebar.multiselect(
+        "Quel continent souhaitez-vous? :warning: :warning: Ce filtre ne fonctionne que dans l'onglet Filtre",
+        options=df.sort_values(by="continent",ascending=False).continent.unique(),
+        #default=df["startYear"].unique(),
+        default=df
+
+    )
 with tab2:    
     st.write('HeatMap de correlation')
     viz_correlation = sns.heatmap(dfdrop.corr(), 
